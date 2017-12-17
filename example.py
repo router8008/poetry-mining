563d74a6cc782340db90e406525d3da829aee795
router <router8008@gmail.com>
2017-12-17 09:51:20 +0800

    change example

--- example.py
+++ example.py
@@ -3,10 +3,15 @@ import os
 from analyzer import Analyzer
 from preprocessor import CutResult, cut_poetry
 
-if __name__ == '__main__':
+
+def example():
     saved_dir = os.curdir
     result = cut_poetry("全唐诗.txt", saved_dir)
     a = Analyzer(result, saved_dir)
-    print(a.find_similar_poet("李世民"))
+    # print(a.find_similar_poet("李世民"))
+
+
+if __name__ == '__main__':
+    example()
 
     # tsne(word_vector, authors)
